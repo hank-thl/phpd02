5ed916fbee5b8dd42bd1049816e821e7caa5aa43
Hank <hank.tsunghanlin@gmail.com>
2022-09-22 11:55:39 +0800

    2209221155

--- main.php
+++ main.php
@@ -14,15 +14,39 @@
 
     $icon = base64_encode($member->icon);
 
-    $sql = "SELECT * FROM food ORDER BY id";
+    // 做分頁 抓出總共有幾筆資料
+    $sql = "SELECT count(*) as `sum` FROM food";
+    $result = $mysqli->query($sql);
+    $data = $result->fetch_object();
+    $total = $data->sum;
+
+
+    define('RPP',10); // 定義一頁有10筆
+    $rpp = RPP;
+    $page = 1;
+    if(isset($_GET['page'])){
+        $page = $_GET['page'];
+    }
+    $prev = $page == 1?1:$page - 1;
+
+    $totalPages = ceil($total / $rpp);
+    $next = $page == $totalPages?$page:$page + 1;
+
+    $start =($page-1)*$rpp;
+
+
+    $sql = "SELECT * FROM food ORDER BY id LIMIT {$start}, {$rpp}";
     $result = $mysqli->query($sql);
 ?>
 Welcome, <?php echo $member->realname ?><br>
 <img style="width:100px" src="data:<?php echo $member->icontype; ?>;base64, <?php echo $icon; ?>">
 <hr>
 <a href="logout.php">Logout</a>
+<hr>
+<a href="?page=<?php echo $prev ?>">上一頁</a> | <a href="?page=<?php echo $next ?>">下一頁</a>
 <table border="1" width="100%">
     <tr>
+        <th>ID</th>
         <th>Name</th>
         <th>Tel</th>
         <th>City</th>
@@ -32,6 +56,7 @@ Welcome, <?php echo $member->realname ?><br>
     <?php
         while($food = $result->fetch_object()){
             echo '<tr>';
+            echo "<td>{$food->id}</td>";
             echo "<td>{$food->name}</td>";
             echo "<td>{$food->tel}</td>";
             echo "<td>{$food->city}</td>";
